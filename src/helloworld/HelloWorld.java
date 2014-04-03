9b35c1193ff1ac814c8c44d4021e1781c16c4ebc
Soe Moe Aye <Soe Moe Aye@194.47.32.37>
2014-04-03 14:15:56 +0200

    initial version

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.beans.EventHandler;
+
+/**
+ *
+ * @author Soe Moe Aye
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
